6d7a9ba3f7294a49701628fa334f87ac9bda6fdf
HS-hand <shs0714@khu.ac.kr>
2026-03-28 17:43:01 +0900

    feat: signup, signin

---
+++ server/core/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+class Settings:
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+settings = Settings()
+supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
