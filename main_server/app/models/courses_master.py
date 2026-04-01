66418f92855375e88a11f1e2c1842c47161b42bc
HS-hand <shs0714@khu.ac.kr>
2026-04-01 21:40:44 +0900

    docs: mentoring_slots 추가, courses_master 수정, curriculum 수정

---
+++ main_server/app/models/courses_master.py
@@ -0,0 +1,8 @@
+# courses_master 테이블
+# - course_id: string (PK, 학수번호)
+# - college_name: string (개설 단과대학)
+# - dept_name: string (개설학과)
+# - course_name: string (과목명)
+# - credits: integer (학점)
+# - course_type: string (이수구분: 전공필수, 전공선택, 교양 등)
+# - created_at: timestamp
