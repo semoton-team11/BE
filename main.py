221ca680acb953e83e07e8ffce0386f93630ba1d
HS-hand <shs0714@khu.ac.kr>
2026-04-03 00:39:53 +0900

    remove

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py - 라우터 등록만 함, DB는 건드리지 않음
-from fastapi import FastAPI
-from routers import profile, scrapbook, connections
-
-app = FastAPI(title="경희대생을 위한 졸업 내비게이션")
-app.include_router(profile.router)
-app.include_router(scrapbook.router)
-app.include_router(connections.router)
